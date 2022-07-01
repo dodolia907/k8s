10fb5ca16f88c1261c21ac226bb13cb4529e92dc
dodolia907 <102458791+dodolia907@users.noreply.github.com>
2022-07-02 01:15:33 +0900

    Update install-ubuntu.sh

---
+++ install-ubuntu.sh
@@ -0,0 +1,52 @@
+cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+apt-get install -y iptables arptables ebtables
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+
+cat > /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+apt-get update && apt-get install -y containerd
+
+mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+
+if grep -q "SystemdCgroup = true" "/etc/containerd/config.toml"; then
+  echo "Config found, skip rewriting..."
+else
+  sed -i -e "s/SystemdCgroup \= false/SystemdCgroup \= true/g" /etc/containerd/config.toml
+fi
+
+systemctl restart containerd
+systemctl enable containerd
+
+apt-get update && apt-get install -y apt-transport-https curl gnupg2
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+systemctl enable kubelet
