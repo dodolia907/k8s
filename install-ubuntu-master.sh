0c495b7222386ed303871a05f163a2881e029f18
dodolia907 <102458791+dodolia907@users.noreply.github.com>
2022-07-02 02:08:37 +0900

    書き直し

--- install-ubuntu-master.sh
+++ install-ubuntu-master.sh
@@ -1,59 +1,54 @@
 swapoff -a
 sed -i -e 's/\/swap.img/#\/swap.img/g' /etc/fstab
+
+modprobe br_netfilter
 cat <<EOF > /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
 sysctl --system
 
-apt-get install -y iptables arptables ebtables
-update-alternatives --set iptables /usr/sbin/iptables-legacy
-update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-update-alternatives --set arptables /usr/sbin/arptables-legacy
-update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+apt install ufw
+systemctl start ufw
+ufw allow 22/tcp
+ufw allow 443/tcp
+ufw allow 6443/tcp
+ufw allow 2379/tcp
+ufw allow 2380/tcp
+ufw allow 10250/tcp
+ufw allow 10251/tcp
+ufw allow 10252/tcp
+ufw allow 30000:32767/tcp
+ufw enable
 
+modprobe overlay
 cat > /etc/modules-load.d/containerd.conf <<EOF
 overlay
 br_netfilter
 EOF
-
-modprobe overlay
-modprobe br_netfilter
-
 cat > /etc/sysctl.d/99-kubernetes-cri.conf <<EOF
 net.bridge.bridge-nf-call-iptables  = 1
 net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 EOF
-
 sysctl --system
 
 apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) \
     stable"
 apt-get update && apt-get install -y containerd.io
-
 mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
-
-if grep -q "SystemdCgroup = true" "/etc/containerd/config.toml"; then
-  echo "Config found, skip rewriting..."
-else
-  sed -i -e "s/SystemdCgroup \= false/SystemdCgroup \= true/g" /etc/containerd/config.toml
-fi
-
 systemctl restart containerd
-systemctl enable containerd
 
-apt-get update && apt-get install -y apt-transport-https curl gnupg2
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
-
-systemctl enable kubelet
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
